da052aad74d93dadb1e4f42e042af19f9d9ce696
Skand Kumar <skandk61@gmail.com>
2023-07-02 03:30:56 +0530

    added heroBanner section

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,37 +1,59 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 import "./styles.scss";
+
+import useFetch from "../../../hooks/useFetch";
+
+import Img from "../../../components/lazyLoadImage/Img";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 
 const HeroBanner = () => {
   const [background, setBackground] = useState("");
   const [query, setQuery] = useState("");
   const navigate = useNavigate();
+  const { url } = useSelector((state) => state.home);
+  const { data, loading } = useFetch("/movie/upcoming");
 
-  const searchQueryhandler = (event) => {
+  useEffect(() => {
+    const bg =
+      url.backdrop +
+      data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+    setBackground(bg);
+  }, [data]);
+
+  const searchQueryHandler = (event) => {
     if (event.key === "Enter" && query.length > 0) {
-      navigate(`./search/${query}`);
+      navigate(`/search/${query}`);
     }
   };
+
   return (
     <div className="heroBanner">
-      <div className="wrapper">
+      {!loading && (
+        <div className="backdrop-img">
+          <Img src={background} />
+        </div>
+      )}
+
+      <div className="opacity-layer"></div>
+      <ContentWrapper>
         <div className="heroBannerContent">
-          <span className="titel">Welcome.</span>
+          <span className="title">Welcome.</span>
           <span className="subTitle">
-            Millions fo movies, TV shows ans people to discover. Explore Now.
+            Millions of movies, TV shows and people to discover. Explore now.
           </span>
           <div className="searchInput">
             <input
               type="text"
-              placeholder="Search for a movie or tv show..."
+              placeholder="Search for a movie or tv show...."
               onChange={(e) => setQuery(e.target.value)}
-              onKeyUp={searchQueryhandler}
+              onKeyUp={searchQueryHandler}
             />
             <button>Search</button>
           </div>
         </div>
-      </div>
+      </ContentWrapper>
     </div>
   );
 };
