54112abc7bbca65fb5f8f3dce18c60dfc8124ca2
Skand Kumar <skandk61@gmail.com>
2023-07-16 02:35:55 +0530

    details page added

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import "./styles.scss";
+import useFetch from "../../hooks/useFetch";
+import { useParams } from "react-router-dom";
+import DetailsBanner from "./detailsBanner/DetailsBanner";
+
 const Details = () => {
-  return <div>Details</div>;
+  // const { data, loading } = useFetch("/movies/{movie}");
+  return (
+    <div>
+      <DetailsBanner />
+    </div>
+  );
 };
 
 export default Details;
